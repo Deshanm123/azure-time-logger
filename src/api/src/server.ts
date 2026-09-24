cf6027732b347b4c5e6abf9a45635505d38129ed
Deshan Maduranga <deshanm123@gmail.com>
2026-09-24 10:00:25 +0530

    Export Fastify app for Vercel runtime

--- src/api/src/server.ts
+++ src/api/src/server.ts
@@ -6,9 +6,4 @@ import { loadConfig } from './config.js';
 const config = loadConfig();
 const app = await buildApp(config, {}, Fastify);
 
-try {
-  await app.listen({ port: config.port, host: config.host });
-} catch (error) {
-  app.log.error(error);
-  process.exit(1);
-}
+export default app;
